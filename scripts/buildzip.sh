4ef02cebde1f2e69d3bf8c08000e76fe7e2a8538
clay <claytantor@gmail.com>
2022-06-25 10:07:15 -0700

    fix buildscript

--- scripts/buildzip.sh
+++ scripts/buildzip.sh
@@ -3,5 +3,5 @@ BASE_DIR=$(pwd)
 VERSION=$1
 BASE_NAME='rapaygo-for-woocommerce'
 rm $BASE_DIR/$BASE_NAME*.zip
-zip -r $BASE_NAME-$VERSION.zip $BASE_DIR/rapaygo-for-woocommerce
+zip -r $BASE_NAME-$VERSION.zip ./rapaygo-for-woocommerce
 cp $BASE_NAME-$VERSION.zip $BASE_NAME.zip
